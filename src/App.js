29038cd4ceadf836beec73977a59213313310351
Mohamed M NourElDin <noureldin@eg.ibm.com>
2021-11-07 06:45:02 +0200

    Render using usePdf hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { PDFViewer } from "@react-pdf/renderer";
+import { PDFViewer, usePDF } from "@react-pdf/renderer";
 import "./App.css";
 import MyDocument from "./MyDocument";
 
-const App = () => (
-  <PDFViewer width="100%">
-    <MyDocument />
-  </PDFViewer>
-);
+const App = () => {
+  const [instance, updateInstance] = usePDF({ document: <MyDocument /> });
+  return (
+    <>
+      <PDFViewer width='100%'>
+        <MyDocument />
+      </PDFViewer>
+      <embed src={instance.url} width='100%' type='application/pdf' />
+    </>
+  );
+};
 
 export default App;
